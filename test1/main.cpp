5ff73c3f9cfd21e80e6bee32e05ea0f6d5b1d759
t-aruna <163091097+t-aruna@users.noreply.github.com>
2024-03-12 09:53:09 +0530

    Add files via upload

---
+++ test1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=2;
+    int b=4;
+
+    cout <<"the number before increment:"<<a<<  endl;
+    a++;
+    cout <<"the number after increment:"<<a<<  endl;
+
+    cout <<"the number before decrement:"<<b<<  endl;
+    b--;
+
+    cout <<"the number after decrement:"<<b<<  endl;
+    return 0;
+}
---
+++ test1b/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    int F;
+    cout << "enter the temp in c:" << endl;
+    cin>>c;
+    F =(c*9/5)+32;
+    cout<<"the temp in c to F is:"<<F<<endl;
+    return 0;
+}
